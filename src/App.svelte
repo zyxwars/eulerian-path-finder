4c594398385fd0ccbc19b000f9e9bdd41e0b3bc2
zyxwars <zyxwars221@gmail.com>
2022-01-31 20:11:04 +0100

    Create node connection gui prototype

---
+++ src/App.svelte
@@ -0,0 +1,58 @@
+<script>
+  import { onMount } from "svelte";
+
+  import Node from "./components/Node.svelte";
+
+  let canvas;
+  let nodes = [];
+  let selectedNode = null;
+
+  onMount(() => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+  });
+
+  const handleClickCanvas = (e) => {
+    nodes = [...nodes, { x: e.clientX, y: e.clientY, isSelected: false }];
+  };
+
+  const handleNodeSelect = (e) => {
+    const newSelectedNode = e.detail;
+
+    if (selectedNode) {
+      const ctx = canvas.getContext("2d");
+
+      ctx.beginPath();
+      ctx.moveTo(selectedNode.x, selectedNode.y);
+      ctx.lineTo(newSelectedNode.x, newSelectedNode.y);
+      ctx.stroke();
+
+      newSelectedNode.isSelected = false;
+      selectedNode.isSelected = false;
+
+      // Refresh state
+      nodes = [...nodes];
+      selectedNode = null;
+      return;
+    }
+
+    selectedNode = newSelectedNode;
+  };
+</script>
+
+<main>
+  <canvas bind:this={canvas} on:click={handleClickCanvas} />
+  {#each nodes as node}
+    <Node {node} on:node_selected={handleNodeSelect} />
+  {/each}
+</main>
+
+<style>
+  main {
+    position: relative;
+    width: 100%;
+    height: 100vh;
+
+    background-color: antiquewhite;
+  }
+</style>
---
+++ src/components/Node.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { createEventDispatcher } from "svelte";
+
+  export let node;
+
+  const dispatch = createEventDispatcher();
+
+  const handleSelect = () => {
+    node.isSelected = true;
+    dispatch("node_selected", node);
+  };
+</script>
+
+<div
+  on:click={handleSelect}
+  class="node"
+  style="left: {node.x}px; top: {node.y}px; background-color: {node.isSelected
+    ? 'red'
+    : 'chartreuse'}"
+/>
+
+<style>
+  .node {
+    position: absolute;
+
+    width: 5rem;
+    height: 5rem;
+  }
+</style>
