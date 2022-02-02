4e6e50e3e69cf79d5afe1a898e309d544c6ce442
zyxwars <zyxwars221@gmail.com>
2022-02-02 14:20:11 +0100

    Add pwa capability

---
+++ public/service-worker.js
@@ -0,0 +1,37 @@
+var cacheName = "euler";
+var contentToCache = [
+  "/index.html",
+  "/global.css",
+  "/build/bundle.css",
+  "/build/bundle.js",
+  "/build/bundle.js.map",
+  "/favicon.png",
+];
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    (async function () {
+      const cache = await caches.open(cacheName);
+      await cache.addAll(contentToCache);
+    })()
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    (async function () {
+      const r = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (r) {
+        return r;
+      }
+      const response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })()
+  );
+});
